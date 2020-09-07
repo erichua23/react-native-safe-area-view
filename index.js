291646e17a410177b3e7c88f443a3b6e758faaf1
erichua <erichua23@gmail.com>
2020-09-08 02:13:29 +0800

    fix bug in react navigation 2 + rn 0.62

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ class SafeView extends Component {
     const WIDTH = isLandscape ? X_HEIGHT : X_WIDTH;
     const HEIGHT = isLandscape ? X_WIDTH : X_HEIGHT;
 
-    this.view._component.measureInWindow((winX, winY, winWidth, winHeight) => {
+    this.view.measureInWindow((winX, winY, winWidth, winHeight) => {
       if (!this.view) {
         return;
       }
